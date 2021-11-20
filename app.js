48781b84c2fc61fc5a812f94d2f4e8176685ee67
Ashton <ashtonfoulger@gmail.com>
2021-11-20 00:05:59 -0700

    added navigation bar

---
+++ app.js
@@ -0,0 +1,9 @@
+//Navigation bar constant variables
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.nav-menu');
+
+//Event Listener for navigation bar responsivness on mobile
+menu.addEventListener('click', function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+})
