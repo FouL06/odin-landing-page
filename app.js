768c345cd055265be9c4bfe9d75c922004abc86d
Ashton <ashtonfoulger@gmail.com>
2021-12-29 11:40:36 -0700

    added projects to bootstrap carousel allowing for nice project showcase

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //Navigation bar constant variables
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.nav-menu');
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".nav-menu");
 
-//Event Listener for navigation bar responsivness on mobile
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
-})
+//Event Listener for navigation bar responsiveness on mobile
+menu.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+});
